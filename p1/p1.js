de9edb6808d901cbe18b061e107e311a6fd9c590
stoffer150 <stoffer@amnesi.dk>
2017-11-22 14:50:46 +0100

    Update p1.js
    Just some minor changes un order to remove the extra div.

--- p1/p1.js
+++ p1/p1.js
@@ -1,30 +1,18 @@
 function add_legs() {
-	
-	var legs_button_div = document.getElementById("legs_button")
-	var legs_button = legs_button_div.getElementsByTagName("button")[0]
-
-	if (legs_button_div.className == "hidden") {
-
-		var legs = document.createElement("img")
+	var legs_button = document.getElementById("legs_button")
+	var legs = document.body.querySelector("#legs")
+	if (legs == null) {
+		legs = document.createElement("img")
 		legs.id = "legs"
 		legs.src = "legs.jpg"
 		legs.style.width = "100%"
 		document.getElementById("strange_body")
-		        .insertBefore(legs, legs_button_div)
-
-		legs_button_div.className = "shown"
+		        .insertBefore(legs, legs_button)
 		legs_button.innerHTML = "HIDE THE HORROR!"
-	
 	}
-	
-	else if (legs_button_div.className == "shown") {
-
-		var legs = document.getElementById("legs")
+	else {
 		legs.parentNode.removeChild(legs)
-
-		legs_button_div.className = "hidden"
 		legs_button.innerHTML = "CHECK MY LEGS OUT"
-	
 	}
 }
 
