b4287d31e3af2253a6cbd4eca84bc28d2a55154c
Asger Andersen <asger.andersen.aa@gmail.com>
2017-12-10 15:43:50 +0100

    p3 - version 1 (only one parent, no additional features)

--- p3/p3.js
+++ p3/p3.js
@@ -1,56 +1,61 @@
 d3.select(window).on('load', init)
 
 function init() {
-    
-    var data = {
-      "name": "A1",
-      "children": [
-        {
-          "name": "B1",
-          "children": [
-            {
-              "name": "C1",
-              "value": 100
-            },
-            {
-              "name": "C2",
-              "value": 300
-            },
-            {
-              "name": "C3",
-              "value": 200
-            }
-          ]
-        },
-        {
-          "name": "B2",
-          "value": 200
-        }
-      ]
-    }
-    
-    makeTree(data)
-
-}
-
-    /*
     d3.json(
         'data.json',
         function(error, data) {
             if (error) throw error;
+            collectChildren(data)
+            data["children"].forEach(collectChildren)
+            delete data["partners"]
+            data["children"].forEach(function(c) {delete c["partners"]})
+            console.log(data)
             makeTree(data)
         }
     )
 }
-*/
+
+function getChildren(partner) {
+    if (partner.hasOwnProperty("children")) {
+        c = partner["children"]
+        c.forEach(function(c) {c["other_parent"] = partner["name"]})
+        return c 
+    }
+    else {
+        return []
+    }
+}
+
+function collectChildren(parent) {
+    var c;
+    if (parent.hasOwnProperty("partners")) {
+        c = parent["partners"].map(p => getChildren(p))
+        c = c.reduce(function(a, b){return a.concat(b)})
+    }
+    else {
+        c = []
+    }
+    parent["children"] = c
+    return
+}
 
 function makeTree(data) {
+
+    var canvas = d3.select("#trump_tree")
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40};
+    var width = +canvas.node().getBoundingClientRect().width - margin.left - margin.right;
+    var height = +canvas.node().getBoundingClientRect().height - margin.top - margin.bottom;
+
     var root = d3.hierarchy(data)
-    //console.log(root)
+
     var treeLayout = d3.tree()
-    treeLayout.size([400,200])
+    treeLayout.size([width,height])
+    
     treeLayout(root)
-    //console.log(root)
+    console.log(root)
+    console.log(root.descendants())
+
     d3.select('svg g.nodes')
       .selectAll('circle.node')
       .data(root.descendants())
@@ -59,7 +64,18 @@ function makeTree(data) {
       .classed('node', true)
       .attr('cx', function(d) {return d.x;})
       .attr('cy', function(d) {return d.y;})
-      .attr('r', 4); 
+      .attr('r', 4);
+
+    d3.select('svg g.nodes')
+      .selectAll('text.node')
+      .data(root.descendants())
+      .enter()
+      .append('text')
+      .classed('node', true)
+      .attr('x', function(d) {return d.x;})
+      .attr('y', function(d) {return d.y + 22;})
+      .text(function(d) {return d.data["name"]})
+
     d3.select('svg g.links')
       .selectAll('line.link')
       .data(root.links())
