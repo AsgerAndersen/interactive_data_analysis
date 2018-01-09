5b896bbfd4c0fd012e9cbe3729ebaed2836e56a3
Stoffer <stoffer@amnesi.dk>
2018-01-09 23:44:46 +0100

    Integeret statistikker lidt bedre ind i systemet, og har fixet node-buggen

--- examproj/script.js
+++ examproj/script.js
@@ -7,20 +7,15 @@ var params = {
     "bin_size": 300,
     "offset": 0,
     "bins": 10,
-    "threshold": -300
+    "threshold": -300,
+    "statistics": [
+        {name: "Average Degree", method: function(links, nodes) { return averageDegree(links, nodes);}},
+        {name: "Number of links", method: function(links, nodes) { return links.length; }},
+        {name: "Number of nodes", method: function(links, nodes) { return nodes.length; }}
+    ]
 };
 
-color = 'black'
-radius = 3
-
-//------------------------------------------------------------------------------
-//Simulate data to use as input for the calculation of the graph statistics,
-//which should be visualized as a step chart
-n_nodes = 800
-max_edges = 1000
-n_bins = 20
-
-function getRandomInt(max) {
+/*function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
@@ -44,12 +39,12 @@ function simulateStepChartData(N) {
     return data
 }
 
-input = simulateStepChartData(n_bins)
+var input = simulateStepChartData(n_bins)*/
 //------------------------------------------------------------------------------
 
 function init() {
 
-    svg = d3.select('svg');
+    svg = d3.select("#vis");
     var margin = {top: 50, right: 50, bottom: 50, left: 50};
     var width = +svg.node().getBoundingClientRect().width - margin.left - margin.right;
     var height = +svg.node().getBoundingClientRect().height - margin.top - margin.bottom;
@@ -58,9 +53,7 @@ function init() {
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
     
-    steps = calcGraphStatistics(input, function(links){return averageDegree(links, n_nodes)})
-    drawStepChart(steps, width, height)
-    /*
+
     d3.csv(
         'data/sodas_data.csv',
         function (error, dat) {
@@ -102,53 +95,64 @@ function init() {
 
         }
     )
-*/
 }
 
 //---------------------------------------------------------------------
 //Calculate the descriptive graph statistics, which should be visualized in the step charts
 
-function calcGraphStatistics(links_sequence, statistic) {
-    statistics = [];
-    for (i=0; i<links_sequence.length; i++) {
-        value = statistic(links_sequence[i])
-        t = i*params.bin_size
-        if (i == 0) {
+function calcGraphStatistics(links, nodes, statistic) {
+    var statistics = [];
+    for (var i=0; i<links.length; i++) {
+        var value = statistic(links[i], nodes[i]);
+        var t = i*params.bin_size;
+        if (i === 0) {
             statistics.push({t: t,
                              value: value,
                              left: true})
         }
         else {
-            last_value = statistics[2*i-2].value 
-            jump = value - last_value
+            var last_value = statistics[2*i-2].value;
+            var jump = value - last_value;
             statistics.push({t: t,
                              value: last_value,
-                             left: false})
+                             left: false});
             statistics.push({t: t,
                              value: value,
                              left: true,
                              jump: jump})
         }
     }
-    statistics.pop()
+    statistics.pop();
     return statistics
 }
 
-function averageDegree(links, n_nodes) {
-    return 2*links.length / n_nodes
+function averageDegree(links, nodes) {
+    return 2*links.length / nodes.length;
 }
 //---------------------------------------------------------------------
 
 //---------------------------------------------------------------------
 //Visualize the descriptive graph statistics in a step chart
 
-function drawStepChart(steps, width, height) {
+function drawStepChart(steps, canvas) {
+
+    var margin = {top: 50, right: 50, bottom: 50, left: 50};
+    var width = +canvas.node().getBoundingClientRect().width - margin.left - margin.right;
+    var height = +canvas.node().getBoundingClientRect().height - margin.top - margin.bottom;
+
+    canvas.selectAll("*")
+        .remove();
+
+    var g = canvas.append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
     var x = d3.scaleLinear()
-        .domain(d3.extent(steps,
+        /*.domain(d3.extent(steps,
             function(d){
                 return d.t;
-            }))
+            }))*/
+        .domain([params.offset, (params.bins - 1) * params.bin_size])
         .range([0,width]);
 
     var y = d3.scaleLinear()
@@ -165,17 +169,17 @@ function drawStepChart(steps, width, height) {
      .enter()
      .append("line")
      .classed("stepgraph-hline", true)
-     .style("stroke", color)
+     .style("stroke", "black")
      .attr("x1", function(d) {return x(d.t)})
      .attr("y1", function(d) {return y(d.value)})
      .attr("x2", function(d) {
            return (x(d.t + params.bin_size))
         })
-     .attr("y2", function(d) {return y(d.value)})
+     .attr("y2", function(d) {return y(d.value)});
     
     g.selectAll(".stepgraph-vline")
      .data(steps.filter(function(d, i){
-            return (d.left && (i != 0))
+            return (d.left && (i !== 0))
          }))
      .enter()
      .append("line")
@@ -187,7 +191,7 @@ function drawStepChart(steps, width, height) {
      .attr("x2", function(d) {return x(d.t)})
      .attr("y2", function(d) {
         return (y(d.value - d.jump))
-      })
+      });
 
     g.selectAll(".stepgraph-circle")
         .data(steps)
@@ -200,12 +204,12 @@ function drawStepChart(steps, width, height) {
         .attr("cy", function(d) {
             return y(d.value) + "px";
         })
-        .attr("r", radius)
+        .attr("r", "3")
         .attr("fill", function(d) {
-            if (d.left) {return color;}
+            if (d.left) {return "black";}
             else {return "white";}
         })
-        .attr("stroke", color)
+        .attr("stroke", "black")
 } 
 //---------------------------------------------------------------------
 
@@ -213,28 +217,51 @@ function drawStepChart(steps, width, height) {
 function calculateGraphs()
 {
     var i = 0;
-    for (var n = 0; n < 1; n++) {
+    for (var n = 0; n < params.bins; n++) {
         var bin = [];
         var looping = true;
         while (looping) {
             var row = data[i];
-            if (row["ts"] < (n+1) * params["bin size"]) {
-                bin.push(data[i]);
+            if (row["ts"] < (n+1) * params["bin_size"]) {
+                bin.push(row);
             } else {
+                console.log("Loop done at:" + i);
                 looping = false;
             }
             i++;
-
         }
-        console.log(i);
         links[n] = calculateLinks(bin, "user", "user2", function(row){
             return row["rssi"] > params['threshold'];//&& row["ts"] > 0 && row["ts"] < 300;
         });
         nodes[n] = simulateNodes(bin);
-        if (n == 0){
+        if (n === 0){
             drawGraph(g, nodes[n], links[n]);
+            for (var k = 0; k < params["statistics"].length; k++) {
+                var divs = d3.select("#stats")
+                    .append("div")
+                    .classed("statistic_div", true)
+                    .attr("id", "stat_div" + k);
+
+                divs.append("span")
+                    .classed("statistic_span", true)
+                    .text(params["statistics"][k].name);
+
+                divs.append("br");
+
+                divs.append("svg")
+                    .classed("statistic_svg", true)
+                    .attr("width", "800")
+                    .attr("height", "200")
+                    .attr("id", "stat" + k);
+            }
         }
-        n++;
+
+        //Update stats progressively
+        for (var j = 0; j < params["statistics"].length; j++) {
+            var canvas = d3.select("#stat" + j);
+            var steps = calcGraphStatistics(links, nodes, params["statistics"][j].method);
+            drawStepChart(steps, canvas)
+        }
     }
 
 
@@ -255,11 +282,19 @@ function calculateLinks(data, node1key, node2key, filter) {
 
 function simulateNodes(data) {
     var nodes = [];
+    var users = [];
     for (var key in data) {
         var row = data[key];
         //---- BUUUUUUGGGGG HERE ----
-        if (nodes.indexOf(row["user"]) < 0) { nodes.push({"id": row["user"]})}
-        if (nodes.indexOf(row["user1"]) < 0) { nodes.push({"id": row["user2"]})}
+        console.log(nodes.indexOf(row["user"]));
+        if (users.indexOf(row["user"]) < 0) {
+            nodes.push({"id": row["user"]});
+            users.push(row["user"]);
+        }
+        if (users.indexOf(row["user2"]) < 0) {
+            nodes.push({"id": row["user2"]});
+            users.push(row["user2"]);
+        }
         //---- BUUUUUUGGGGG HERE ----
     }
     return nodes;
